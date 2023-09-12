e83442ab8bb12f4c98135f66eb3c097c4f1b2155
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-12 10:15:02 -0300

    first commit

---
+++ CoreDataMVVM/CoreDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMApp.swift
+//  CoreDataMVVM
+//
+//  Created by thaxz on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
