6c77dad4d35e8c325e030fd7c6bf69310afc8847
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-12 10:39:21 -0300

    feature(CoreData): Storing fetch entities inside a publisher

--- CoreDataMVVM/ViewModel/CoreDataViewModel.swift
+++ CoreDataMVVM/ViewModel/CoreDataViewModel.swift
@@ -12,15 +12,30 @@ import CoreData
 class CoreDataViewModel: ObservableObject {
     
     let container: NSPersistentContainer
+    // To store our entities
+    @Published var savedEntities: [FruitEntity] = []
     
     init() {
         // linking and loading our container
-        self.container = NSPersistentContainer(name: "")
+        self.container = NSPersistentContainer(name: "FruitsContainer")
         container.loadPersistentStores { description, error in
             if let error = error {
                 // handle error
                 print("error loading core data \(error.localizedDescription)")
             }
+        }
+        fetchFruits()
+    }
+    
+    func fetchFruits(){
+        // creating request
+        let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+        // making the request
+        do {
+            // saving in a place that our views are subscribed to
+            savedEntities = try container.viewContext.fetch(request)
+        } catch {
+            print("error fetching \(error)")
         }
     }
     
