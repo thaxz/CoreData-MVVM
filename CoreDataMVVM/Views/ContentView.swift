5063cc2b17c8fa86e76828130900c586fafd9b31
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-12 10:59:28 -0300

    feature(CoreData): Creating mock UI

---
+++ CoreDataMVVM/Views/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  CoreDataMVVM
+//
+//  Created by thaxz on 12/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = CoreDataViewModel()
+    @State var text: String = ""
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 20){
+                TextField("Add fruits here", text: $text)
+                    .font(.headline)
+                    .padding(.leading)
+                    .frame(height: 55)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                Spacer()
+                Button {
+                    guard !text.isEmpty else {return}
+                    viewModel.addFruit(text: text)
+                    text = ""
+                } label: {
+                    Text("Save")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                List {
+                    ForEach(viewModel.savedEntities){ entity in
+                        Text(entity.name ?? "NO NAME")
+                    }
+                } .listStyle(.plain)
+            }
+            .padding(.horizontal)
+            .navigationTitle("Fruits")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
